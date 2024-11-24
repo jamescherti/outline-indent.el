86f1ea4be8a0fa1f498bf317dcf54467033fd75b
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-11-24 14:11:38 -0500

    Release: 1.1.0

--- outline-indent.el
+++ outline-indent.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.6
+;; Version: 1.1.0
 ;; URL: https://github.com/jamescherti/outline-indent.el
 ;; Keywords: outlines
 ;; Package-Requires: ((emacs "26.1"))
