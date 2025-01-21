427428a3c5f189afc7b77928d877aea3716c24f5
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-01-21 15:19:21 -0500

    Update copyright year range

--- outline-indent.el
+++ outline-indent.el
@@ -1,6 +1,6 @@
 ;;; outline-indent.el --- Folding text based on indentation (origami alternative) -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2025 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 1.1.0
